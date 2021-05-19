b6feeca0dbdb84e3f21a7ea0ac7d61330ed33ee8
Samer-Alnajjar <samerk.alnajjar@gmail.com>
2021-05-19 16:35:23 +0300

    Fix lab31

--- src/component/todo/form.js
+++ src/component/todo/form.js
@@ -13,7 +13,7 @@ const TodoForm = (props) => {
     setItem({ ...item, [e.target.name]: e.target.value });
     // this.setState({ item: {...this.state.item, [e.target.name]: e.target.value } });
   };
-  
+
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -60,38 +60,6 @@ const TodoForm = (props) => {
           Add Item
         </Button>
       </Form>
-      {/* <h3>Add Item</h3>
-      <form onSubmit={handleSubmit}>
-        <label>
-          <span>To Do Item</span>
-          <input
-            name="text"
-            placeholder="Add To Do List Item"
-            onChange={handleInputChange}
-          />
-        </label>
-        <label>
-          <span>Difficulty Rating</span>
-          <input
-            defaultValue="1"
-            type="range"
-            min="1"
-            max="5"
-            name="difficulty"
-            onChange={handleInputChange}
-          />
-        </label>
-        <label>
-          <span>Assigned To</span>
-          <input
-            type="text"
-            name="assignee"
-            placeholder="Assigned To"
-            onChange={handleInputChange}
-          />
-        </label>
-        <button>Add Item</button>
-      </form> */}
     </>
   );
 };
