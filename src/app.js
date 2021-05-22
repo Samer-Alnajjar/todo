27fee8f06cd9c0394d2aa4b75ac481baeac6c25e
Samer-Alnajjar <samerk.alnajjar@gmail.com>
2021-05-22 16:34:32 +0300

    Lab34

--- src/app.js
+++ src/app.js
@@ -1,19 +1,32 @@
-import React from "react";
+import React, {useContext} from "react";
+import { If, Else, Then } from 'react-if';
 import ToDo from "./component/todo/todo";
-import NavBar from './component/todo/navbar';
-import './app.css';
+import NavBar from "./component/todo/navbar";
+import "./app.css";
 import PaginationProvider from "./component/context/pagination";
 
+// import Auth from "./component/auth/auth.js";
+import { LoginContext } from "./component/auth/context.js";
+
 const App = () => {
+  const loginContext = useContext(LoginContext);
   return (
     <>
       <NavBar />
-        <PaginationProvider>
-          <ToDo />
-        </PaginationProvider>
+      <If condition={loginContext.loggedIn}>
+        <Then>
+          <PaginationProvider>
+            <ToDo />
+          </PaginationProvider>
+        </Then>
+        <Else>
+          <div></div>
+        </Else>
+      </If>
+      {/* <EditLink />
+      <DeleteLink /> */}
     </>
   );
 };
-
 
 export default App;
